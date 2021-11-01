646da58b2079aecfb2011a34411b4660772a7712
minthukha99 <minthukha99@gmail.com>
2021-11-01 08:20:28 +0800

    Create Navigation.vue

---
+++ src/components/Navigation.vue
@@ -0,0 +1,43 @@
+<template>
+<!-- edited Min 22.01, only done til 25.36 cause no img icon-->
+<header>
+  
+    <nav class = "container">
+        <div class = "branding">
+            <router-link class ="header" :to="{name:'Home'}">FireBlogs</router-link>
+        </div>
+        <div class="nav-link">
+            <ul>
+                <router-link class ="link" to="#">Home </router-link>
+                <router-link class ="link" to="#">Blog </router-link>
+                <router-link class ="link" to="#">Create Post </router-link>
+                <router-link class ="link" to="#">Login/Register </router-link>
+            </ul>
+        </div>
+    </nav>
+    <menuIcon/> 
+    <transition name = "mobile-nav">
+        <ul>
+            <router-link class ="link" to="#">Home </router-link>
+            <router-link class ="link" to="#">Blog </router-link>
+            <router-link class ="link" to="#">Create Post </router-link>
+            <router-link class ="link" to="#">Login/Register </router-link>
+        </ul>
+    </transition>
+
+</header>
+</template>
+
+<script>
+import menuIcon from '../assets/Icons/bars-regular.svg';
+export default {
+    name : 'navigation',
+    components: {
+        menuIcon
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
