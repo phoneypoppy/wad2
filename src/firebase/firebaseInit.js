ee46498643a0c9d9dae56e7a6663c5f3426beee0
minthukha99 <minthukha99@gmail.com>
2021-11-01 08:20:30 +0800

    Update firebaseInit.js

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -1,1 +1,17 @@
 // file that holds firebase config 
+
+
+// @phoeni add/edit below 
+import { apply } from "core-js/fn/reflect";
+import firebase from firebase/apply;
+import "firebase/firestore";
+
+var firebaseConfig = {
+
+}
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+export {timestamp};
+export default firebaseApp.firestore();
+
