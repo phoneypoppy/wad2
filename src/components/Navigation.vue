bf795f215312e497b83072ef51157b7e6eec2ce6
minthukha99 <minthukha99@gmail.com>
2021-11-01 23:34:35 +0800

    Update Navigation.vue

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -8,21 +8,21 @@
                 <router-link class="header" :to="{ name: 'Home' }"> GameBlogs </router-link>
             </div>
             <div class="nav-links">
-                <ul>
-                    <router-links class="link" to="#">Home</router-links>
-                    <router-links class="link" to="#">Blogs</router-links>
-                    <router-links class="link" to="#">Create Post</router-links>
-                    <router-links class="link" to="#">Login/Register</router-links>
+                <ul v-show="!mobile">
+                    <router-link class="link" to="#">Home</router-link>
+                    <router-link class="link" to="#">Blogs</router-link>
+                    <router-link class="link" to="#">Create Post</router-link>
+                    <router-link class="link" to="#">Login/Register</router-link>
                 </ul>
             </div>
         </nav>
-        <menuIcon/>
+        <menuIcon @click="toggleMobileNav" class ="menu-icon" v-show="mobile" />
         <transition name="mobile-nav">
-                <ul>
-                    <router-links class="link" to="#">Home</router-links>
-                    <router-links class="link" to="#">Blogs</router-links>
-                    <router-links class="link" to="#">Create Post</router-links>
-                    <router-links class="link" to="#">Login/Register</router-links>
+                <ul class = "mobile-nav" v-show="mobileNav">
+                    <router-link class="link" to="#">Home</router-link>
+                    <router-link class="link" to="#">Blogs</router-link>
+                    <router-link class="link" to="#">Create Post</router-link>
+                    <router-link class="link" to="#">Login/Register</router-link>
                 </ul>
         </transition>
     </header>
@@ -35,6 +35,32 @@ export default {
     name: 'navigation',
     components: {
         menuIcon
+    },
+    data(){
+        return {
+            mobile: null,
+            mobileNav: null,
+            windowWidth: null,
+        };
+    },
+    created(){
+        window.addEventListener('resize',this.checkScreen);
+        this.checkScreen();
+    },
+    methods:{
+        checkScreen(){
+            this.windowWidth = window.innerWidth;
+            if (this.windowWidth<= 750){
+                this.mobile = true;
+                return;
+            }
+            this.mobile= false;
+            this.mobileNav = false;
+            return;
+        },
+        toggleMobileNav(){
+            this.mobileNav = !this.mobileNav;
+        }
     }
 };
 
@@ -72,6 +98,71 @@ export default {
                     text-decoration: none;
                 }
             }
+
+            .nav-links{
+                position: relative;
+                display : flex;
+                flex: 1;
+                align-items : center;
+                justify-content: flex-end;
+
+
+                ul{
+                    margin-right: 32px;
+
+                    .link{
+                        margin-right: 32px;
+                    }
+
+                    .link:last-child{
+                        margin-right: 0;
+                    }
+                }
+
+            }
+        }
+
+        .menu-icon {
+            cursor : pointer;
+            position : absolute;
+            top : 32px;
+            right : 25px ;
+            height: 25px;
+            width : auto;
+        }
+
+        .mobile-nav{
+            padding:20px;
+            width: 70%;
+            max-width: 250px ;
+            display : flex;
+            flex-direction: column;
+            position : fixed;
+            height: 100%;
+            background-color: #3030;
+            top: 0;
+            left : 0;
+
+            .link{
+                padding : 15px 0;
+                color :white
+            }
+        }
+        .mobile-nav-enter-active,
+        .mobile-nav-leave-active{
+            transition : all 1s ease;
+        }
+
+        .mobile-nav-enter{
+            transform: translateX(-250px);
+        }
+
+        .mobile-nav-enter-to{
+            transform: translateX(0);
+        }
+
+        .mobile.nav-leave-to{
+            transform: translateX(-250px);
         }
     }
 </style>
