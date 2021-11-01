d9dfc884ba4dc61dc1e2e852d44e527f9679f1f1
minthukha99 <minthukha99@gmail.com>
2021-11-02 00:54:02 +0800

    Update BlogPost.vue

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -1,10 +1,13 @@
 <template>
-  <div></div>
+  <div class="blog-wrapper">
+      <div class="blog-content"></div>
+  </div>
 </template>
 
 <script>
 export default {
     name: "blogPost",
+    props: ["posts"],
 
 }
 </script>
