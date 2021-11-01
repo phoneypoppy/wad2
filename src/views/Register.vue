a0ae844f9986f5c84f9a25b2040c33b3882cf255
minthukha99 <minthukha99@gmail.com>
2021-11-02 04:10:11 +0800

    Create Register.vue

---
+++ src/views/Register.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class ="form-wrap">
+        <form class = "register">
+            <p class =  "login-register">
+                Dont have account?
+                <router-link class ="router-link" :to="{name: 'Register'}"> Register</router-link>
+            </p>
+            <h2>Login to xxx </h2>
+            <div class="inputs">
+                <div class="input">
+                    <input type = "text" placeholder="FirstName" v-model="firstName">
+                    <user class = "icon" />
+                </div>
+                <div class="input">
+                    <input type = "text" placeholder="Last Name" v-model="lastName">
+                    <user class = "icon" />
+                </div>
+                <div class="input">
+                    <input type = "text" placeholder="Username" v-model="username">
+                    <user class = "icon" />
+                </div>
+                <div class="input">
+                    <input type = "text" placeholder="Email" v-model="email">
+                    <email class = "icon" />
+                </div>
+    
+                <div class="input">
+                    <input type = "password" placeholder="Password" v-model="password">
+                    <password class = "icon" />
+                </div>
+            </div>      
+            <router-link class="forgot-password" :to="{name: 'ForgotPasword'}">Forgot pw</router-link>
+            <button> Sigin </button> 
+            <div class="angle"></div>
+        </form>
+        <div class="background"></div>
+  </div>
+</template>
+
+<script>
+import email from "../assets/Icons/evelope-regular.svg"
+import password from "../assets/Icons/lock-alt-solid.svg"
+import user from "../assets/Icons/user-alt-light.svg"
+export default {
+    name:"Register",
+    components:{
+        email,
+        password,
+        user,
+    
+    }
+
+}
+</script>
+
+<style>
+
+</style>
